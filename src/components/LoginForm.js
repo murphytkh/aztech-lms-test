b36ff4eaa76bd6b0abec6ea9fe9b931ac2c2836f
murphytkh <murphytkh@hotmail.com>
2021-03-15 13:50:28 +0800

    updated timeline, code cleanup for login/signup done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,6 +6,7 @@ import BG from "../resources/login/nastuh-abootalebi-284883-unsplash@2x.png";
 import LoginRect from "../resources/login/Rectangle 2@2x.png";
 import Line from "../resources/login/Path 1.svg";
 import Logo from "../resources/login/Aztech logo 2020-WH.svg";
+import Button from "../resources/login/NEXT button@2x.png";
 
 function LoginForm(props)
 {
@@ -13,9 +14,8 @@ function LoginForm(props)
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSubmitNext(e)
+    function handleNext()
     {
-        e.preventDefault();
         console.log({email});
         console.log({password});
         history.push("/dashboard")
@@ -42,15 +42,16 @@ function LoginForm(props)
             <img alt = "" src = {BG} className = "main-bg"></img>
             {/* footer */}
             <div className = "main-footer">
-                <h1 className = "main-footer-center">COPYRIGHT © 2020 AZTECH TECHNOLOGIES PTE LTD. ALL RIGHTS RESERVED.</h1>
-                <h1 className = "main-footer-right">PRIVACY POLICY · TERMS & CONDITIONS</h1>
+                <h1 className = "center">COPYRIGHT © 2020 AZTECH TECHNOLOGIES PTE LTD. ALL RIGHTS RESERVED.</h1>
+                <h1 className = "right">PRIVACY POLICY · TERMS & CONDITIONS</h1>
             </div>
             {/* login form */}
             <div className = "login-form">
                 {/* labels and images */}
-                <img alt = "" src = {LoginRect} className = "login-form-bg"></img>
-                <img alt = "" src = {Logo} className = "login-form-logo"></img>
-                <img alt = "" src = {Line} className = "login-form-line"></img>
+                <img alt = "" src = {LoginRect} className = "bg"></img>
+                <img alt = "" src = {Logo} className = "logo"></img>
+                <img alt = "" src = {Line} className = "line"></img>
+
                 <h1>Welcome Back!</h1>
                 <h2>LOGIN</h2>
                 <h3>Lighting Management System (LMS)</h3>
@@ -60,28 +61,32 @@ function LoginForm(props)
                     By clicking on next, you acknowledge that you have read and accepted the Terms of Service and 
                     the Privacy Policy.
                 </h6>
+                
                 {/* inputs */}
-                <form onSubmit = {handleSubmitNext}>
-                    <input
-                        type = "text"
-                        id = "login-email"
-                        className = "login-form-email"
-                        name = "email"
-                        value = {email}
-                        placeholder = "EMAIL"
-                        onChange = {handleChangeEmail}
-                    ></input>
-                    <input
-                        type = "password"
-                        id = "login-password"
-                        className = "login-form-password"
-                        name = "password"
-                        value = {password}
-                        placeholder = "PASSWORD"
-                        onChange = {handleChangePassword}
-                    ></input>
-                    <button type = "submit" className = "login-form-nextbtn"></button>
-                </form>
+                <input
+                    type = "text"
+                    id = "login-email"
+                    className = "input0"
+                    name = "email"
+                    value = {email}
+                    placeholder = "EMAIL"
+                    onChange = {handleChangeEmail}
+                ></input>
+                <input
+                    type = "password"
+                    id = "login-password"
+                    className = "input1"
+                    name = "password"
+                    value = {password}
+                    placeholder = "PASSWORD"
+                    onChange = {handleChangePassword}
+                ></input>
+                <img 
+                    alt = "" 
+                    src = {Button} 
+                    className = "btn"
+                    onClick = {handleNext}
+                ></img>
             </div>
         </div>
     );
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -6,20 +6,20 @@ import BG from "../resources/login/nastuh-abootalebi-284883-unsplash@2x.png";
 import LoginRect from "../resources/login/Rectangle 2@2x.png";
 import Line from "../resources/login/Path 1.svg";
 import Logo from "../resources/login/Aztech logo 2020-WH.svg";
+import Button from "../resources/login/NEXT button@2x.png";
 
 function SignUpForm(props)
 {
     const history = useHistory();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [verifypassword, setVerifyPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");
 
-    function handleSubmitNext(e)
+    function handleNext()
     {
-        e.preventDefault();
         console.log({email});
         console.log({password});
-        console.log({verifypassword});
+        console.log({verifyPassword});
     }
 
     function handleLogin(e)
@@ -45,57 +45,64 @@ function SignUpForm(props)
 
     return (
         <div>
+            {/* backgorund image */}
             <img alt = "" src = {BG} className = "main-bg"></img>
+            {/* footer */}
             <div className = "main-footer">
-                <h1 className = "main-footer-center">COPYRIGHT © 2020 AZTECH TECHNOLOGIES PTE LTD. ALL RIGHTS RESERVED.</h1>
-                <h1 className = "main-footer-right">PRIVACY POLICY · TERMS & CONDITIONS</h1>
+                <h1 className = "center">COPYRIGHT © 2020 AZTECH TECHNOLOGIES PTE LTD. ALL RIGHTS RESERVED.</h1>
+                <h1 className = "right">PRIVACY POLICY · TERMS & CONDITIONS</h1>
             </div>
-            <div className = "signup-page-form">
-                <img alt = "" src = {LoginRect} className = "signup-page-form-rect"></img>
-                <h1 className = "signup-page-form-welcome">Welcome to</h1>
-                <img alt = "" src = {Logo} className = "signup-page-form-logo"></img>
-                <h1 className = "signup-page-form-signuptext">SIGN UP</h1>
-                <h1 className = "signup-page-form-lmstext">Lighting Management System (LMS)</h1>
-                <form onSubmit = {handleSubmitNext}>
-                    <input
-                        type = "text"
-                        id = "signup-email"
-                        className = "signup-page-form-email"
-                        name = "email"
-                        value = {email}
-                        placeholder = "EMAIL"
-                        onChange = {handleChangeEmail}
-                    ></input>
-                    <input
-                        type = "password"
-                        id = "signup-password"
-                        className = "signup-page-form-password"
-                        name = "password"
-                        value = {password}
-                        placeholder = "PASSWORD"
-                        onChange = {handleChangePassword}
-                    ></input>
-                    <input
-                        type = "password"
-                        id = "signup-verifypassword"
-                        className = "signup-page-form-verifypassword"
-                        name = "verifypassword"
-                        value = {verifypassword}
-                        placeholder = "VERIFY PASSWORD"
-                        onChange = {handleChangeVerifyPassword}
-                    ></input>
-                    <button type = "submit" className = "signup-page-form-nextbtn"></button>
-                </form>
-                <form onSubmit = {handleLogin}>
-                    <h1 className = "signup-page-form-logintext">ALREADY REGISTERED?</h1>
-                    <button type = "submit" className = "signup-page-form-loginbtn">
-                        <h1 className = "signup-page-form-loginbtntext">LOGIN</h1>
-                    </button>
-                </form>
-                <img alt = "" src = {Line} className = "signup-page-form-line"></img>
-                <h2 className = "signup-page-form-agreetext">
-                    By clicking on next, you acknowledge that you have read and accepted the Terms of Service and the Privacy Policy.
-                </h2>
+            {/* signup form */}
+            <div className = "signup-form">
+                {/* labels and images */}
+                <img alt = "" src = {LoginRect} className = "bg"></img>
+                <img alt = "" src = {Logo} className = "logo"></img>
+                <img alt = "" src = {Line} className = "line"></img>
+
+                <h1>Welcome to</h1>
+                <h2>SIGN UP</h2>
+                <h3>Lighting Management System (LMS)</h3>
+                <h4>LOGIN</h4>
+                <h5 onClick = {handleLogin}>ALREADY REGISTERED?</h5>
+                <h6>
+                    By clicking on next, you acknowledge that you have read and accepted the Terms of Service and 
+                    the Privacy Policy.
+                </h6>
+                
+                {/* inputs */}
+                <input
+                    type = "text"
+                    id = "signup-email"
+                    className = "input0"
+                    name = "email"
+                    value = {email}
+                    placeholder = "EMAIL"
+                    onChange = {handleChangeEmail}
+                ></input>
+                <input
+                    type = "password"
+                    id = "signup-password"
+                    className = "input1"
+                    name = "password"
+                    value = {password}
+                    placeholder = "PASSWORD"
+                    onChange = {handleChangePassword}
+                ></input>
+                <input
+                    type = "password"
+                    id = "signup-verifypassword"
+                    className = "input2"
+                    name = "verifypassword"
+                    value = {verifyPassword}
+                    placeholder = "VERIFY PASSWORD"
+                    onChange = {handleChangeVerifyPassword}
+                ></input>
+                <img 
+                    alt = "" 
+                    src = {Button} 
+                    className = "btn"
+                    onClick = {handleNext}
+                ></img>
             </div>
         </div>
     );
