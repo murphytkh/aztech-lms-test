7ccc77156ef17ffb32abec86bef2b79d0bba1ad6
murphytkh <murphytkh@hotmail.com>
2021-03-29 18:13:55 +0800

    added location to status table, imported edit icon

--- src/components/LightStatus.js
+++ src/components/LightStatus.js
@@ -7,12 +7,14 @@ import TableSortButton from "./TableSortButton";
 
 import HeaderIcon from "../resources/view/status-header-icon.svg";
 import RefreshIcon from "../resources/dashboard/icon-refresh-black.svg";
+import EditIcon from "../resources/view/status-edit-icon.svg";
 
 class LightStatusObject
 {
-    constructor(name, date, time, status)
+    constructor(name, location, date, time, status)
     {
         this.name = name;
+        this.location = location;
         this.date = date;
         this.time = time;
         this.status = status;
@@ -60,7 +62,7 @@ function LightStatus(props)
         .slice(currentPage * 10, (currentPage + 1) * 10)
         .map(lightStatus =>
             <tr key = {lightStatus.name}>
-                <td className = "name">{lightStatus.name}</td>
+                <td className = "name">{lightStatus.name} - {lightStatus.location}</td>
                 <td className = "date">{lightStatus.date}</td>
                 <td className = "time">{lightStatus.time}</td>
                 <td className = "status">{lightStatus.status}</td>
@@ -98,35 +100,35 @@ function LightStatus(props)
         // simulate getting data
         var a = [];
 
-        a.push(new LightStatusObject("1.1.1", "2020-09-01", "17:44:00", "ON"));
-        a.push(new LightStatusObject("1.1.2", "2020-09-02", "17:44:01", "OFF"));
-        a.push(new LightStatusObject("1.1.3", "2020-09-03", "17:44:03", "ON"));
-        a.push(new LightStatusObject("1.1.4", "2020-09-04", "17:44:04", "ON"));
-        a.push(new LightStatusObject("1.1.5", "2020-09-05", "17:44:05", "OFF"));
-        a.push(new LightStatusObject("1.1.6", "2020-09-06", "17:44:06", "ON"));
-        a.push(new LightStatusObject("1.1.7", "2020-09-07", "17:44:07", "OFF"));
-        a.push(new LightStatusObject("1.1.8", "2020-09-08", "17:44:08", "ON"));
-        a.push(new LightStatusObject("1.1.9", "2020-09-09", "17:44:09", "OFF"));
-        a.push(new LightStatusObject("1.2.1", "2020-09-10", "17:44:10", "ON"));
-        a.push(new LightStatusObject("1.2.2", "2020-09-11", "17:44:11", "OFF"));
-        a.push(new LightStatusObject("1.2.3", "2020-09-12", "17:44:12", "ON"));
-        a.push(new LightStatusObject("1.2.4", "2020-09-13", "17:44:13", "OFF"));
-        a.push(new LightStatusObject("1.2.5", "2020-09-14", "17:44:14", "ON"));
-        a.push(new LightStatusObject("1.2.6", "2020-09-15", "17:44:15", "OFF"));
-        a.push(new LightStatusObject("1.2.7", "2020-09-16", "17:44:16", "ON"));
-        a.push(new LightStatusObject("1.2.8", "2020-09-17", "17:44:17", "OFF"));
-        a.push(new LightStatusObject("1.2.9", "2020-09-18", "17:44:18", "ON"));
-        a.push(new LightStatusObject("1.3.1", "2020-09-19", "17:44:19", "OFF"));
-        a.push(new LightStatusObject("1.3.2", "2020-09-20", "17:44:20", "ON"));
-        a.push(new LightStatusObject("1.3.3", "2020-09-21", "17:44:21", "OFF"));
-        a.push(new LightStatusObject("1.3.4", "2020-09-22", "17:44:22", "ON"));
-        a.push(new LightStatusObject("1.3.5", "2020-09-23", "17:44:23", "OFF"));
-        a.push(new LightStatusObject("1.3.6", "2020-09-24", "17:44:24", "ON"));
+        a.push(new LightStatusObject("1.1.1", "Front row, nearest to HR", "2020-09-01", "17:44:00", "ON"));
+        a.push(new LightStatusObject("1.1.2", "Location undefined", "2020-09-02", "17:44:01", "OFF"));
+        a.push(new LightStatusObject("1.1.3", "Location undefined", "2020-09-03", "17:44:03", "ON"));
+        a.push(new LightStatusObject("1.1.4", "Location undefined", "2020-09-04", "17:44:04", "ON"));
+        a.push(new LightStatusObject("1.1.5", "Location undefined", "2020-09-05", "17:44:05", "OFF"));
+        a.push(new LightStatusObject("1.1.6", "Location undefined", "2020-09-06", "17:44:06", "ON"));
+        a.push(new LightStatusObject("1.1.7", "Location undefined", "2020-09-07", "17:44:07", "OFF"));
+        a.push(new LightStatusObject("1.1.8", "Location undefined", "2020-09-08", "17:44:08", "ON"));
+        a.push(new LightStatusObject("1.1.9", "Location undefined", "2020-09-09", "17:44:09", "OFF"));
+        a.push(new LightStatusObject("1.2.1", "Location undefined", "2020-09-10", "17:44:10", "ON"));
+        a.push(new LightStatusObject("1.2.2", "Location undefined", "2020-09-11", "17:44:11", "OFF"));
+        a.push(new LightStatusObject("1.2.3", "Location undefined", "2020-09-12", "17:44:12", "ON"));
+        a.push(new LightStatusObject("1.2.4", "Location undefined", "2020-09-13", "17:44:13", "OFF"));
+        a.push(new LightStatusObject("1.2.5", "Location undefined", "2020-09-14", "17:44:14", "ON"));
+        a.push(new LightStatusObject("1.2.6", "Location undefined", "2020-09-15", "17:44:15", "OFF"));
+        a.push(new LightStatusObject("1.2.7", "Location undefined", "2020-09-16", "17:44:16", "ON"));
+        a.push(new LightStatusObject("1.2.8", "Location undefined", "2020-09-17", "17:44:17", "OFF"));
+        a.push(new LightStatusObject("1.2.9", "Location undefined", "2020-09-18", "17:44:18", "ON"));
+        a.push(new LightStatusObject("1.3.1", "Location undefined", "2020-09-19", "17:44:19", "OFF"));
+        a.push(new LightStatusObject("1.3.2", "Location undefined", "2020-09-20", "17:44:20", "ON"));
+        a.push(new LightStatusObject("1.3.3", "Location undefined", "2020-09-21", "17:44:21", "OFF"));
+        a.push(new LightStatusObject("1.3.4", "Location undefined", "2020-09-22", "17:44:22", "ON"));
+        a.push(new LightStatusObject("1.3.5", "Location undefined", "2020-09-23", "17:44:23", "OFF"));
+        a.push(new LightStatusObject("1.3.6", "Location undefined", "2020-09-24", "17:44:24", "ON"));
         // 24
 
         // + 40
         for (var i = 0; i < 40; ++i)
-            a.push(new LightStatusObject("1.4." + i.toString(), "2020-09-25", "17:44:25", (i % 2) ? "ON" : "OFF"));
+            a.push(new LightStatusObject("1.4." + i.toString(), "Location undefined", "2020-09-25", "17:44:25", (i % 2) ? "ON" : "OFF"));
 
         setLightStatusData(a);
         setDisplayLength(a.length < 10 ? a.length : 10);
