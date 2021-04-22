e1c9eb870b4d9e1c33ea4267af6e0205f1227f6a
murphytkh <murphytkh@hotmail.com>
2021-04-22 17:43:43 +0800

    buttons added, not formatted

--- src/components/three/UIConfigTrigger.js
+++ src/components/three/UIConfigTrigger.js
@@ -2,10 +2,36 @@ import React from "react";
 
 function UIConfigTrigger(props)
 {
+    function handleEdit()
+    {
+
+    }
+
+    function handleViewData()
+    {
+
+    }
+
     return(
         <div className = "block" id = "trigger">
             <div className = "label">Triggers: </div>
             {/* buttons */}
+            <div 
+                className = "three-btn trigger-edit"
+                //id = {showColour ? "" : "disabled"}
+                onClick = {handleEdit}
+                style = {{backgroundColor: "#7F849F"}}
+            >
+                EDIT
+            </div>
+            <div 
+                className = "three-btn trigger-data" 
+                //id = {group !== "" ? "" : "disabled"}
+                onClick = {handleViewData}
+                style = {{backgroundColor: "#7F849F"}}
+            >
+                VIEW DATA
+            </div>
         </div>
     );
 }
